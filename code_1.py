de6fd45b2499707501054351fa396e74801cadc5
Bhavish Dhanda <bhavish.dhanda@students.mq.edu.au>
2023-05-23 21:05:31 +1000

    Added Steps

--- code_1.py
+++ code_1.py
@@ -1,1 +1,19 @@
 print("Write code here hahaha")
+
+# Step 1
+# This is the api call from the Module with the data from the temperature senser and the occupancy data
+
+# Step 2
+# Correlation of data from BOM and the Module
+
+# Step 3
+# Processing of data including training and testing of model
+
+# Step 4
+# Creation of DB and storing the data 
+
+# Step 5
+# Optimizing the data for the output
+
+# Step 6
+# Send the response with the output
